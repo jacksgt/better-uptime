a7c15efbfd299f673ad1d9d60a872b17bad70e82
Jack H <jackhenschel97@gmail.com>
2015-03-17 19:49:34 +0100

    Added basic C source code for better-uptime

---
+++ uptime.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+FILE *fp;
+
+int main() {    
+  char line [128];
+  char * token;
+  int seconds;
+
+  int days, hours, minutes;
+
+  fp = fopen ("/proc/uptime", "r");
+    
+  if(fp == NULL) {
+    fprintf(stderr, "Could not open \"/proc/uptime\"\n");
+    return 1;
+  }
+
+
