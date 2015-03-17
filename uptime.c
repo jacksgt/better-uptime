e07ab26cd66d91a90897f81fe212870fafbe69b5
Jack H <jackhenschel97@gmail.com>
2015-03-17 21:23:46 +0100

    First usable commit

--- uptime.c
+++ uptime.c
@@ -7,14 +7,48 @@ int main() {
   char line [128];
   char * token;
   int seconds;
+  char daysStr[] = "days";
+  char hoursStr[] = "hours";
+  char minutesStr[] = "minutes";
 
   int days, hours, minutes;
 
+  /* read content of /proc/uptime (provided by the kernel */
   fp = fopen ("/proc/uptime", "r");
     
   if(fp == NULL) {
     fprintf(stderr, "Could not open \"/proc/uptime\"\n");
     return 1;
   }
+    while(fgets(line, 128, fp) != NULL) {
+
+    /* split the line at the space */
+    token = strtok(line, " ");
+
+    /* remove last three characters of the string */
+    /* accuracy of ".XX" is not required */
+    token[strlen(token)-3] = '\0';
+
+    /* convert token to int */
+    seconds = strtol(token,NULL,10);
+
+    /* begin arithmetic operations */
+    days = seconds/86000;
+    seconds %= 86400;
+
+    hours = seconds/3600;
+    seconds %= 3600;
+
+    minutes = seconds/60;
+    /* seconds %= 60; */
+    /* not required because this (should) equal 0 anyway */
+
+    /* print out the results */
+    printf("%d day(s), %d hour(s), %d minute(s)\n", days, hours, minutes);
+  }
+    
+  fclose(fp);
+  return 0;
+
 }
 
